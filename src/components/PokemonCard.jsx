c72f732f694599bc9f8e11f994912d776a8af3ed
Wan2302 <erwan.cherel35@hotmail.com>
2023-03-28 15:37:46 +0200

    React Basics 04: props.1

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,6 @@
 export default PokemonCard;
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-function PokemonCard() {
-
-    const pokemon = pokemonList[0];
+function PokemonCard({ pokemon }) {
 
     return <figure>
     {pokemon.hasOwnProperty('imgSrc') ? (
