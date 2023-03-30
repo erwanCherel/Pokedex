65a8c1e0d48864d52367f6248602da01e3b505b8
Wan2302 <erwan.cherel35@hotmail.com>
2023-03-30 10:57:09 +0200

    React Basics 08 rendering-list.1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,25 +2,13 @@ export default NavBar;
 
 function NavBar({pokemonIndex, setPokemonIndex, pokemonList}) {
 
-  const handleClickPrevious = () => {
-    setPokemonIndex(pokemonIndex - 1);
-    if (pokemonIndex === 3) {
-      alert("pika pikachu !!!")
-    }
-  };
-
-  const handleClickNext = () => {
-    setPokemonIndex(pokemonIndex + 1);
-    if (pokemonIndex === 3) {
-      alert("pika pikachu !!!")
-    }
-  };
-
-
     return (
-        <>
-            <button id='previousBtn' onClick={handleClickPrevious} disabled={(pokemonIndex === 0 ? true : false)} >Previous</button>
-            <button id='nextBtn' onClick={handleClickNext} disabled={(pokemonIndex === pokemonList.length - 1 ? true : false)}>Next</button>
-        </>
+        <ul>
+          {pokemonList.map((pokemon, pokemonIndex) => (
+            <button onClick={() => setPokemonIndex(pokemonIndex)} key={pokemonIndex}>
+              {pokemon.name}
+            </button>
+          ))}
+        </ul>
     )
 }
